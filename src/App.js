d81e323df4519816f7de0d5bdf00408f4141fd51
Marjie <marjie.hensz@gmail.com>
2018-03-11 15:09:50 -0500

    added links for firebase

--- src/App.js
+++ src/App.js
@@ -2,6 +2,17 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+  // Initialize Firebase
+  // let config = {
+  //   apiKey: "AIzaSyAjFruoklcMh8H6tJMMzJ_JU-3LH9x-ODs",
+  //   authDomain: "stash-it-e75bd.firebaseapp.com",
+  //   databaseURL: "https://stash-it-e75bd.firebaseio.com",
+  //   projectId: "stash-it-e75bd",
+  //   storageBucket: "stash-it-e75bd.appspot.com",
+  //   messagingSenderId: "499464668272"
+  // };
+  // firebase.initializeApp(config);
+
 class App extends Component {
   render() {
     return (
@@ -11,7 +22,7 @@ class App extends Component {
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          To get started, edit <code>src/App.js</code> and save to reload. Testing.
         </p>
       </div>
     );
