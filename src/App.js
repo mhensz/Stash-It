9dcf3ace5d97e65c1f072fd3a3b43f9b3f799a85
Marjie Hensz <marjie.hensz@gmail.com>
2018-03-13 21:28:34 -0500

    added firebase and ui. not working yet

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import * as firebaseui from'firebaseui';
+var firebase = require('firebase');
 
-  // Initialize Firebase
-  // let config = {
-  //   apiKey: "AIzaSyAjFruoklcMh8H6tJMMzJ_JU-3LH9x-ODs",
-  //   authDomain: "stash-it-e75bd.firebaseapp.com",
-  //   databaseURL: "https://stash-it-e75bd.firebaseio.com",
-  //   projectId: "stash-it-e75bd",
-  //   storageBucket: "stash-it-e75bd.appspot.com",
-  //   messagingSenderId: "499464668272"
-  // };
-  // firebase.initializeApp(config);
+  //Initialize Firebase
+  let config = {
+    apiKey: "AIzaSyAjFruoklcMh8H6tJMMzJ_JU-3LH9x-ODs",
+    authDomain: "stash-it-e75bd.firebaseapp.com",
+    databaseURL: "https://stash-it-e75bd.firebaseio.com",
+    projectId: "stash-it-e75bd",
+    storageBucket: "stash-it-e75bd.appspot.com",
+    messagingSenderId: "499464668272"
+  };
+  firebase.initializeApp(config);
 
 class App extends Component {
   render() {
