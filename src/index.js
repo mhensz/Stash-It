adf81beb3d6e65ffc345f49d859c6c5e1545ee3a
Marjie Hensz <marjie.hensz@gmail.com>
2018-03-29 20:28:52 -0500

    started adding router.  not working yet

--- src/index.js
+++ src/index.js
@@ -3,6 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import { Router, Route } from 'react-router'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+/*ReactDOM.render(<App />, document.getElementById("root"));*/
+ReactDOM.render((
+    <Router history={hashHistory}>
+        <Route path="/" component={App}/>
+    </Router>
+), document.getElementById('app'))  
 registerServiceWorker();
